2067f0e41f57f98bbb07026ff52de7f896b983d8
Janine Hempy <jhempy@gmail.com>
2017-10-17 10:22:17 -0400

    Independent component behavior

--- app/main.js
+++ app/main.js
@@ -1,8 +1,25 @@
+Vue.component('message', {
+  template: "<div>{{ markers[answer%2] }} <button v-on:click='add'>Click me!</button></div>",
+  data: function() {
+    return {
+      answer: 0,
+      markers: ['X', 'O']
+    };
+  },
+  methods: {
+    add: function() {
+      this.answer++;
+    }
+  }
+});
+
 var app = new Vue({
   el: "#app",
   data: {
-    vueShowMessage: true,
-    vueMessage: 'Hello again from Vue'
+    vueShowMessage1: true,
+    vueShowMessage2: true,
+    vueMessage1: 'Hello from Vue',
+    vueMessage2: 'Hello again from Vue'
   },
   methods: {
 
